1f8c288bdf11b4464d4b2b4d78674cfcb669ca70
Axel Rigaud <axel.rigaud@gmail.com>
2015-03-29 23:43:38 +0200

    portfolio

--- js/main.js
+++ js/main.js
@@ -1,31 +1,88 @@
 var apiKey  = 'zdinRP5GJarvCOa3PWiQxYb94dPyc9Xx';
 var userID  = 'hellovincent';
 
-var $container = $('.portfolio-container')
+(function() {
+    var behanceProjectsAPI = 'http://www.behance.net/v2/users/'+ userID +'/projects?callback=?&api_key='+ apiKey;
+    var behanceProjectAPI = function(id){
+    	return 'http://www.behance.net/v2/projects/'+id+'?callback=?&api_key='+ apiKey;
+    };
+    function setPortfolioTemplate() {
+        var projectsData = JSON.parse(sessionStorage.getItem('behanceProjects')),
+            rawTemplate = $('#portfolio-template').html(),
+            template    = Handlebars.compile(rawTemplate),
+            result      = template(projectsData);
+        $('#projects-grid').html(result);
+    };
 
-$container.isotope({
-  itemSelector: '.portfolio-item',
-  layoutMode: 'fitRows',
-});
+    function setViewerTemplate(id){
+    	var projectData = JSON.parse(sessionStorage.getItem('behanceProject'+id)),
+    		rawTemplate = $('#viewer-template').html(),
+    		template = Handlebars.compile(rawTemplate),
+    		result = template(projectData.project);
 
-$('.illustration-filter').click(function(){
-	$container.isotope({filter: '.illustration'});
-});
+    	$('#project-viewer').html(result);
+    	$('html, body').animate({
+        scrollTop: $("#project-viewer").offset().top
+    }, 500);
 
-$('.portfolio-item').click(function(){
-	$container.fadeOut('slow',function(){
-		target='portfolio';
-		scrollTo(target,function(){
-			$.get('./portfolio/showpackage.html',function(data){
-		$('section.portfolio').html(data);
-		
+    };
+
+    function showProject(id){
+	    if(sessionStorage.getItem('behanceProject'+id)){
+	    	setViewerTemplate(id);
+	    }
+	    else{
+	    	$.getJSON(behanceProjectAPI(id),function(project){
+	    		var data = JSON.stringify(project);
+	    		sessionStorage.setItem('behanceProject'+id,data);
+	    		setViewerTemplate(id);
+	    	})
+	    }
+    }
+
+    if(sessionStorage.getItem('behanceProjects')) {
+        setPortfolioTemplate();
+    } else {
+        $.getJSON(behanceProjectsAPI, function(projects) {
+            var data = JSON.stringify(projects);
+            sessionStorage.setItem('behanceProjects', data);
+            setPortfolioTemplate();
+        });
+    };
+
+    $('a').on('click',function(e){
+    	e.preventDefault();
+    	showProject($(this).data("project_id"));
+    })
+
+    var $container = $('.portfolio-container')
+
+	$container.isotope({
+	  itemSelector: '.portfolio-item',
+	  layoutMode: 'fitRows',
+	});
+
+	$('.illustration-filter').click(function(){
+		$container.isotope({filter: '.illustration'});
+	});
+
+	$('.portfolio-item').click(function(){
+		$container.fadeOut('slow',function(){
+			target='portfolio';
+			scrollTo(target,function(){
+				$.get('./portfolio/showpackage.html',function(data){
+			$('section.portfolio').html(data);
+			
+			});
+			
+			
+		});
 		});
 		
-		
-	});
-	});
-	
-})
+	})
+
+})();
+
 
 function scrollTo(target,callback){
 	$('html, body').animate( { scrollTop: $('.'+target).offset().top }, 750 );
